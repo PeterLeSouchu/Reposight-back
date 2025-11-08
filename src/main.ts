9004be6fa50d2c97077c197bdd2f791de24eb0de
Peter Le Souchu <peter22510@gmail.com>
2025-11-08 12:16:44 +0100

    debug

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'https://reposight-front.vercel.app',
+    origin: 'https://reposight-front.vercel.app',
     credentials: true,
   });
   // console.log('process.env.FRONTEND_URL', process.env.FRONTEND_URL);
